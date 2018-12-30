a29408cddab1cb2a1251f658cb4c5e2c7ce22ca0
jorge <spartanomau1988@hotmail.com>
2018-12-30 00:02:19 -0500

    Se realizaron cometarios de registro de cambioes 3 en  IngInformatica.java

--- src/inginformatica/IngInformatica.java
+++ src/inginformatica/IngInformatica.java
@@ -81,6 +81,7 @@ public class IngInformatica {
 		System.out.println("Archivo modificado satisfactoriamente..");
                 System.out.println("Sera registrado los cambios en este proyecto");
                 System.out.println("Sera registrado los cambios en este proyecto 2");
+                System.out.println("Sera registrado los cambios en este proyecto 3333");
  
 		} catch (IOException e) {
 		} finally {
